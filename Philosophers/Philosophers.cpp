1110fe9996a089b8c9d8585538cba9336e54657d
Yaroslav Abrosimov <abrosimovyar@gmail.com>
2023-02-13 16:43:20 +0300

    Проблема с последним потоком, почему-то выходит за перделы вектора по какой-то причине

--- Philosophers/Philosophers.cpp
+++ Philosophers/Philosophers.cpp
@@ -1,54 +1,125 @@
-#include <thread>
+//#include <thread>
+//#include <iostream>
+//#include <string>
+//#include <mutex>
+//
+//using namespace std;
+//using namespace chrono;
+//
+//
+//
+//void waiting(bool* forks, int leftFork, int rightFork, string tabulation, string philisopherName, int loopIteration) {
+//	if (forks[leftFork] && forks[rightFork]) {
+//		forks[leftFork] = !forks[leftFork];
+//		forks[rightFork] = !forks[rightFork];
+//		eating(forks, leftFork, rightFork, loopIteration, tabulation, philisopherName);
+//
+//	}
+//	else thinking(tabulation, philisopherName, loopIteration);
+//	cout << endl;
+//}
+//
+//int main() {
+//	bool forks[] = { true, true, true, true, true };
+//
+//	for (int i = 0; i < 20; i++) {
+//		thread philisopherA(waiting, forks, 0, 1, "\t", "A",i);
+//		thread philisopherB(waiting, forks, 1, 2, "\t\t", "B",i);
+//		thread philisopherC(waiting, forks, 2, 3, "\t\t\t", "C",i);
+//		thread philisopherD(waiting, forks, 3, 4, "\t\t\t\t", "D",i);
+//		thread philisopherE(waiting, forks, 4, 0, "\t\t\t\t\t", "E",i);
+//
+//		philisopherA.join();
+//		philisopherB.join();
+//		philisopherC.join();
+//		philisopherD.join();
+//		philisopherE.join();
+//
+//		cout << endl;
+//	}
+//
+//	system("pause");
+//}
+
 #include <iostream>
 #include <string>
-#include <mutex>
+#include <vector>
+#include <thread>
+#include <random>
+#include <time.h>
 
 using namespace std;
 using namespace chrono;
 
-void eating(bool* forks, int positionOfLeftFork, int positionOfRightFork, int loopIteration, string tabulation, string philisopherName) {
-	string out = "[" + to_string(loopIteration) + "]" + tabulation + "Philosopher " + philisopherName + " is eating.\n";
+class Fork {
+public:
+	bool isFree = true;
+};
+
+void eating(vector<Fork>& forks, int leftFork, int rightFork, int philosopher) {
+	string tabulation(philosopher, '\t');
+	string out = tabulation + "Philosopher " + to_string(philosopher) + " is eating.\n";
 	cout << out;
-	this_thread::sleep_for(milliseconds(10));
-	forks[positionOfLeftFork] = !forks[positionOfLeftFork];
-	forks[positionOfRightFork] = !forks[positionOfRightFork];
+	forks[leftFork].isFree = !forks[leftFork].isFree;
+	forks[rightFork].isFree = !forks[rightFork].isFree;
 }
 
-void thinking(string tabulation, string philisopherName, int loopIteration) {
-	string out = "[" + to_string(loopIteration) + "]" + tabulation + "Philosopher " + philisopherName + " is thinking.\n";
+void thinking(int philosopher) {
+	string tabulation(philosopher, '\t');
+	string out = "Philosopher " + to_string(philosopher) + " is thinking.\n";
 	cout << out;
-	this_thread::sleep_for(milliseconds(10));
 }
 
-void waiting(bool* forks, int positionOfLeftFork, int positionOfRightFork, string tabulation, string philisopherName, int loopIteration) {
-	if (forks[positionOfLeftFork] && forks[positionOfRightFork]) {
-		forks[positionOfLeftFork] = !forks[positionOfLeftFork];
-		forks[positionOfRightFork] = !forks[positionOfRightFork];
-		eating(forks, positionOfLeftFork, positionOfRightFork, loopIteration, tabulation, philisopherName);
+void waiting(vector<Fork>& forks, int leftFork, int rightFork, int philosopher, int tabulation) {
+	int seconds = 1;
+	auto endTimer = time(NULL) + seconds;
 
+	random_device random;
+	uniform_int_distribution<int> int_distribution(0, 100);
+
+	int delay = int_distribution(random);
+	this_thread::sleep_for(microseconds(delay));
+
+	if (rightFork > forks.size())
+		rightFork = 0;
+
+	while (time(NULL) < endTimer) {
+		if (forks[leftFork].isFree && forks[rightFork].isFree) {
+			forks[leftFork].isFree = !forks[leftFork].isFree;
+			forks[rightFork].isFree = !forks[rightFork].isFree;
+			string tab(philosopher, '\t');
+			string out = tab + to_string(philosopher) + " is eating.\n";
+			cout << out;
+			forks[leftFork].isFree = !forks[leftFork].isFree;
+			forks[rightFork].isFree = !forks[rightFork].isFree;
+		}
+		else {
+			string tab(philosopher, '\t');
+			string out = tab + to_string(philosopher) + " is thinking.\n";
+			cout << out;
+		}
+		cout << endl;
 	}
-	else thinking(tabulation, philisopherName, loopIteration);
-	cout << endl;
 }
 
 int main() {
-	bool forks[] = { true, true, true, true, true };
+	int numOfPhilisophers;
+	vector<thread> vectorOfPhilosophers;
+	vector<Fork> vectorOfForks;
 
-	for (int i = 0; i < 20; i++) {
-		thread philisopherA(waiting, forks, 0, 1, "\t", "A",i);
-		thread philisopherB(waiting, forks, 1, 2, "\t\t", "B",i);
-		thread philisopherC(waiting, forks, 2, 3, "\t\t\t", "C",i);
-		thread philisopherD(waiting, forks, 3, 4, "\t\t\t\t", "D",i);
-		thread philisopherE(waiting, forks, 4, 0, "\t\t\t\t\t", "E",i);
+	cout << "Input number of philisophers: ";
+	cin >> numOfPhilisophers;
+	cout << "\n";
 
-		philisopherA.join();
-		philisopherB.join();
-		philisopherC.join();
-		philisopherD.join();
-		philisopherE.join();
+	for (int i = 0; i < numOfPhilisophers; i++)
+		vectorOfForks.emplace_back();
 
-		cout << endl;
+	for (int i = 0; i < numOfPhilisophers; i++) {
+		vectorOfPhilosophers.emplace_back(waiting, ref(vectorOfForks), i, i + 1, i + 1, i);
 	}
 
+	for (thread& thread : vectorOfPhilosophers) {
+		thread.join();
+	}
 	system("pause");
 }
